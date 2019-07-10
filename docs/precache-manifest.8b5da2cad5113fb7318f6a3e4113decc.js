0c839c5e4e878f918875fbf6cd6dfad2a4059e68
Chara <37601555+lightlyss@users.noreply.github.com>
2019-07-10 12:57:27 -0400

    configure gh-pages deployment

---
+++ docs/precache-manifest.8b5da2cad5113fb7318f6a3e4113decc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657d1ebda5891b1ab6df96e8e6871ab5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ee4c4a262c33ea7a3f9",
+    "url": "./static/css/2.d6dfce3d.chunk.css"
+  },
+  {
+    "revision": "adeff8fbe6400bbe8f39",
+    "url": "./static/css/main.4e807c84.chunk.css"
+  },
+  {
+    "revision": "4ee4c4a262c33ea7a3f9",
+    "url": "./static/js/2.09707496.chunk.js"
+  },
+  {
+    "revision": "adeff8fbe6400bbe8f39",
+    "url": "./static/js/main.55660e8c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "./static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "./static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "./static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "./static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "./static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "./static/media/icons-20.90247cfd.ttf"
+  }
+]);
